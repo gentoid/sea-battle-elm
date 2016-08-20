66312283dac97791854fb1ed3d5d5e9e3c6b1112
Viktor Lazarev <taurus101v@gmail.com>
2016-08-20 19:45:36 +0400

    Extract Board' module

---
+++ Board.elm
@@ -0,0 +1,35 @@
+module Board exposing (..)
+
+import Collage exposing (..)
+import Element exposing (..)
+
+import Field
+
+freeSpace : Float
+freeSpace  = 20
+
+type alias Model =
+  { myField : Field.Model
+  , otherField : Field.Model
+  }
+
+initialModel : Model
+initialModel =
+  { myField = Field.initialModel Field.My
+  , otherField = Field.initialModel Field.Opponent
+  }
+
+fieldToForm : Field.Model -> Form
+fieldToForm field =
+  case field.side of
+    Field.My ->
+      Field.toForm field |> moveX (-(field.width + freeSpace) / 2)
+
+    Field.Opponent ->
+      Field.toForm field |> moveX ((field.width + freeSpace) / 2)
+
+toElement : Model -> Element
+toElement model =
+  [ model.myField, model.otherField ]
+    |> List.map fieldToForm
+    |> collage (round (freeSpace * 3 + model.myField.width + model.otherField.width)) (round (freeSpace * 2 + model.myField.width))
--- game.elm
+++ game.elm
@@ -1,15 +1,8 @@
 import Html exposing (..)
 import Html.App as App
--- import Collage exposing (toForm, Form, rect, square)
-import Collage exposing (..)
-import Element exposing (..)
-import Field
--- import Ship
--- import Html.Attributes exposing (..)
--- import Html.Events exposing (..)
--- import Http
--- import Task
--- import Json.Decode as Json
+import Element exposing (toHtml)
+
+import Board
 
 main : Program Never
 main =
@@ -21,17 +14,12 @@ main =
     }
 
 type alias Model =
-  { myField : Field.Model
-  , otherField : Field.Model
+  { board : Board.Model
   }
-
-freeSpace : Float
-freeSpace  = 20
 
 init : (Model, Cmd Msg)
 init =
-  ( { myField = Field.initialModel Field.My
-    , otherField = Field.initialModel Field.Opponent
+  ( { board = Board.initialModel
     }
   , Cmd.none
   )
@@ -45,20 +33,9 @@ update msg model =
     None ->
       (model, Cmd.none)
 
-fieldToForm : Field.Model -> Form
-fieldToForm field =
-  case field.side of
-    Field.My ->
-      Field.toForm field |> moveX (-(field.width + freeSpace) / 2)
-
-    Field.Opponent ->
-      Field.toForm field |> moveX ((field.width + freeSpace) / 2)
-
 view : Model -> Html Msg
 view model =
-  [ model.myField, model.otherField ]
-    |> List.map fieldToForm
-    |> collage (round (freeSpace * 3 + model.myField.width + model.otherField.width)) (round (freeSpace * 2 + model.myField.width))
+  Board.toElement model.board
     |> toHtml
 
 subscriptions : Model -> Sub Msg
